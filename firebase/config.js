0a2bbed0d35a393d0c6a42ff53c2d2cdbb21dd93
Khrystyna Yaruta <kh.yaruta@gmail.com>
2025-03-18 14:05:23 +0100

    Fix firebase imports

--- firebase/config.js
+++ firebase/config.js
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp, getAuth } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
